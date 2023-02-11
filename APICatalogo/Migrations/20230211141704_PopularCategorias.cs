1651d94b274cdc4de7bfbec7bfa1facef8adab3e
Thomaz de Moura <tmoura@4bio.com.br>
2023-02-11 11:47:02 -0300

    [feature] adicao das migrations para fazer ajustes nas tabelas utilizando o data annotation e para popular as tabelas de categoria e produto.

---
+++ APICatalogo/Migrations/20230211141704_PopularCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome, ImagemURL) Values('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome, ImagemURL) Values('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome, ImagemURL) Values('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
